9f1cdeb475cf862ab7b8b30d36b6c49a17994ff5
Jason S Morris <jsmorris@gmail.com>
2012-01-02 07:49:03 +0000

    got nodify working on cloud9

--- app.js
+++ app.js
@@ -3,8 +3,8 @@
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes')
+var express = require('express'), 
+    routes = require('./routes');
 
 var app = module.exports = express.createServer();
 
@@ -31,5 +31,5 @@ app.configure('production', function(){
 
 app.get('/', routes.index);
 
-app.listen(3000);
+app.listen(process.env.PORT, '0.0.0.0');
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
